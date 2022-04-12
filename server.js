6eccedd09b810238c6abb60360f5f112f92e4942
nicosampler <nf.dominguez.87@gmail.com>
2022-04-12 08:30:29 -0300

    port 8080

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/.well-known/acme-challenge/:id', function(req, res, next) {
     res.send(req.params.id+'.'+certbotId);
 });
 
-const server = app.listen(80, function () {
+const server = app.listen(8080, function () {
    const host = server.address().address
    const port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
